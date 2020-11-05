d0c4a45277c59c39cb675c7d7e7550a2872b2a63
1song2 <1song2918@gmail.com>
2020-11-05 22:30:44 +0900

    readLine 적용 완료 (리팩토링 필요)

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -7,21 +7,19 @@
 
 import Foundation
 
-//let input = readLine() ?? ""
+let input = readLine() ?? ""
 
 struct LadderGame {
-//    func createGame() -> String {
-//        guard let numberOfPlayers: Int = Int(input) else {
-//            fatalError("게임에 참여할 인원을 숫자로 입력해주세요.")
-//        }
-//        var message = ""
-//        if (numberOfPlayers < 2 || numberOfPlayers > 8) {
-//            message = "인원이 너무 적거나 많아 게임을 시작할 수 없습니다."
-//        } else {
-//            //message = printLadder(numberOfPlayers, height: 4)
-//        }
-//        return message
-//    }
+    func createGame() {
+        guard let numberOfPlayers: Int = Int(input) else {
+            fatalError("게임에 참여할 인원을 숫자로 입력해주세요.")
+        }
+        if (numberOfPlayers < 2 || numberOfPlayers > 8) {
+            print("인원이 너무 적거나 많아 게임을 시작할 수 없습니다.")
+        } else {
+            printLadder(array: self.createLadder(numberOfPlayers, height: 4))
+        }
+    }
     
     func printLadder(array: Array<Array<String>>) {
         for innerArray in array {
@@ -60,4 +58,6 @@ struct LadderGame {
 var newGame = LadderGame()
 //print(newGame.createGame())
 //print(newGame.printLadder(3, height: 4))
-newGame.printLadder(array: newGame.createLadder(6, height: 4))
+//newGame.printLadder(array: newGame.createLadder(6, height: 4))
+
+newGame.createGame()

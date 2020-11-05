3b9a7f7f00eb622452aa08a99fe91e7f92ffffb0
1song2 <1song2918@gmail.com>
2020-11-05 18:22:23 +0900

    struct 'LadderGame', method 'createGame' 생성

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -7,5 +7,27 @@
 
 import Foundation
 
-print("Hello, World!")
+let input = readLine() ?? ""
+
+struct LadderGame {
+    func createGame() -> String {
+        guard let player: Int = Int(input) else {
+            fatalError("게임에 참여할 인원을 숫자로 입력해주세요.")
+        }
+        var message = ""
+        if (player < 2 || player > 8) {
+            message = "인원이 너무 적거나 많아 게임을 시작할 수 없습니다."
+        } else {
+            printLadder()
+        }
+        return message
+    }
+    
+    func printLadder() {
+        
+    }
+}
+
+var newGame = LadderGame()
+print(newGame.createGame())
 

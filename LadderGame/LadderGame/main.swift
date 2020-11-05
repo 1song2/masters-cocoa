2f427ee0e3c7cea017e6a1ed2ee1bff8affd89c0
1song2 <1song2918@gmail.com>
2020-11-05 22:23:14 +0900

    사다리 출력 구현 (readLine 함수 적용 필요)

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -7,27 +7,57 @@
 
 import Foundation
 
-let input = readLine() ?? ""
+//let input = readLine() ?? ""
 
 struct LadderGame {
-    func createGame() -> String {
-        guard let player: Int = Int(input) else {
-            fatalError("게임에 참여할 인원을 숫자로 입력해주세요.")
+//    func createGame() -> String {
+//        guard let numberOfPlayers: Int = Int(input) else {
+//            fatalError("게임에 참여할 인원을 숫자로 입력해주세요.")
+//        }
+//        var message = ""
+//        if (numberOfPlayers < 2 || numberOfPlayers > 8) {
+//            message = "인원이 너무 적거나 많아 게임을 시작할 수 없습니다."
+//        } else {
+//            //message = printLadder(numberOfPlayers, height: 4)
+//        }
+//        return message
+//    }
+    
+    func printLadder(array: Array<Array<String>>) {
+        for innerArray in array {
+            for item in innerArray {
+                print(item, terminator: " ")
+            }
+            print()
         }
-        var message = ""
-        if (player < 2 || player > 8) {
-            message = "인원이 너무 적거나 많아 게임을 시작할 수 없습니다."
-        } else {
-            printLadder()
-        }
-        return message
     }
     
-    func printLadder() {
-        
+    func createLadder(_ players: Int, height: Int) -> Array<Array<String>> {
+        var resultArray = Array<Array<String>>()
+        for _ in 1...height {
+            resultArray.append(putStep(players))
+        }
+        return resultArray
+    }
+    
+    func putStep(_ players: Int) -> Array<String> {
+        var step = Array<String>()
+        for i in 1...(players - 1 + players) {
+            if i % 2 != 0 {
+                step.append("|")
+            } else {
+                if Int.random(in: 0...100) % 2 == 0 {
+                    step.append("-")
+                } else {
+                    step.append(" ")
+                }
+            }
+        }
+        return step
     }
 }
 
 var newGame = LadderGame()
-print(newGame.createGame())
-
+//print(newGame.createGame())
+//print(newGame.printLadder(3, height: 4))
+newGame.printLadder(array: newGame.createLadder(6, height: 4))

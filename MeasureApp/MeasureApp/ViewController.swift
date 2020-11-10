b54c778ab4e8d7a4ce702ca0bc4e74d805863f50
1song2 <1song2918@gmail.com>
2020-11-10 17:55:56 +0900

    Convert To CM, M, Ft 구현

--- MeasureApp/MeasureApp/ViewController.swift
+++ MeasureApp/MeasureApp/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        outputLabel.text = ""
     }
 
-
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+        if let inputText = inputTextField.text {
+            if let inputNumber = Double(inputText) {
+                let unitValue = Measurement(value: inputNumber, unit: UnitLength.inches)
+                
+                outputLabel.text = "\(unitValue.converted(to: UnitLength.centimeters))"
+            }
+        }
+    }
+    
+    @IBAction func convertedToMButtonPressed(_ sender: UIButton) {
+        if let inputText = inputTextField.text {
+            if let inputNumber = Double(inputText) {
+                let unitValue = Measurement(value: inputNumber, unit: UnitLength.inches)
+                
+                outputLabel.text = "\(unitValue.converted(to: UnitLength.meters))"
+            }
+        }
+    }
+    
+    @IBAction func convertedToFeetButtonPressed(_ sender: UIButton) {
+        if let inputText = inputTextField.text {
+            if let inputNumber = Double(inputText) {
+                let unitValue = Measurement(value: inputNumber, unit: UnitLength.inches)
+                
+                outputLabel.text = "\(unitValue.converted(to: UnitLength.feet))"
+            }
+        }
+    }
+    
 }
 

333a2647daea326ff0ba8a15294d8906570c06dc
1song2 <1song2918@gmail.com>
2020-11-11 19:08:54 +0900

    연습문제4 - 단어 맞추기 앱 완성

--- WordPairsQuiz/WordPairsQuiz/ViewController.swift
+++ WordPairsQuiz/WordPairsQuiz/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var currentWordLabel: UILabel!
+    @IBOutlet weak var nextWordLabel: UILabel!
+    @IBOutlet weak var inputWordTextField: UITextField!
+    
+    var currentIndex = 0
+    let wholeWordsArray = [
+        "동해물과",
+        "백두산이",
+        "마르고",
+        "닳도록",
+        "하느님이",
+        "보우하사",
+        "우리나라",
+        "만세",
+        "무궁화",
+        "삼천리",
+    ].shuffled()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        inputWordTextField.delegate = self
+        currentWordLabel.text = wholeWordsArray[currentIndex]
+        nextWordLabel.text = wholeWordsArray[currentIndex + 1]
+        print(wholeWordsArray)
     }
-
-
+    
+    @IBAction func checkButtonPressed(_ sender: UIButton) {
+        if inputWordTextField.text == currentWordLabel.text {
+            currentIndex += 1
+            currentWordLabel.text = nextWordLabel.text
+            currentWordLabel.textColor = .black
+            inputWordTextField.text = ""
+            if currentIndex >= 8 {
+                nextWordLabel.text = ""
+            } else {
+                nextWordLabel.text = wholeWordsArray[currentIndex + 1]
+            }
+        } else {
+            currentWordLabel.textColor = .red
+            inputWordTextField.text = ""
+        }    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}

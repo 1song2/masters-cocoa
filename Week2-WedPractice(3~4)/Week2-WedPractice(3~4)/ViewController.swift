0ace3286c80e690a9f6b9c36f6d2a677cb7af79c
1song2 <1song2918@gmail.com>
2020-11-11 14:39:00 +0900

    Initial Commit

---
+++ Week2-WedPractice(3~4)/Week2-WedPractice(3~4)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week2-WedPractice(3~4)
+//
+//  Created by Song on 2020/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

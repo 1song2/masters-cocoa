1ccbcda0ed00c324e94be87dcbd45ffd5c4ad3be
1song2 <1song2918@gmail.com>
2020-11-11 19:11:56 +0900

    연습문제3 - 비밀번호 입력앱 완성

---
+++ PasswordValidator/PasswordValidator/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  PasswordValidator
+//
+//  Created by Song on 2020/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var levelView: UIView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        passwordInput.placeholder = "비밀번호를 입력하세요"
+    }
+    
+    func passwordValidator(password: String) -> Int {
+        var condition = (length: false, alphabet: false, number: false, symbol: false)
+        let alphabet = CharacterSet.letters
+        let number = CharacterSet.decimalDigits
+        var symbol = CharacterSet()
+        symbol.insert(charactersIn: "-+!@#$%")
+        
+        condition.length = password.count >= 8
+        condition.alphabet = (password.rangeOfCharacter(from: alphabet) != nil)
+        condition.number = (password.rangeOfCharacter(from: number) != nil)
+        condition.symbol = (password.rangeOfCharacter(from: symbol) != nil)
+        
+        switch condition {
+        case (length: true, alphabet: true, number: true, symbol: true):
+            return 5
+        case (length: true, alphabet: true, number: true, symbol: false):
+            return 4
+        case (length: false, alphabet: true, number: false, symbol: false):
+            return 2
+        case (length: false, alphabet: false, number: true, symbol: false):
+            return 1
+        default:
+            return 3
+        }
+    }
+    
+    @IBAction func validateButtonPressed(_ sender: UIButton) {
+        switch passwordValidator(password: passwordInput.text ?? "") {
+        case 1:
+            levelView.backgroundColor = .red
+            descriptionLabel.text = "아주 약한 암호"
+        case 2:
+            levelView.backgroundColor = .orange
+            descriptionLabel.text = "약한 암호"
+        case 3:
+            levelView.backgroundColor = .yellow
+            descriptionLabel.text = "보통 암호"
+        case 4:
+            levelView.backgroundColor = .blue
+            descriptionLabel.text = "강한 암호"
+        case 5:
+            levelView.backgroundColor = .green
+            descriptionLabel.text = "아주 강한 암호"
+        default:
+            levelView.backgroundColor = .systemGray
+            descriptionLabel.text = "암호 수준"
+        }
+    }
+}
+

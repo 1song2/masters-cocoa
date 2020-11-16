eb5a45e87bcff241e7c773a6f9d40c7ee4350283
1song2 <1song2918@gmail.com>
2020-11-16 14:55:29 +0900

    연습문제2 완성

---
+++ Week3-MonPractice/Week3-MonPractice/exercise2 - binaryNumber.swift
@@ -0,0 +1,32 @@
+//
+//  exercise2 - binaryNumber.swift
+//  Week3-MonPractice
+//
+//  Created by Song on 2020/11/16.
+//
+
+import Foundation
+
+struct BinaryNumber {
+    private var values: Array<String>
+    /// computed property
+    var count: Int {
+        return values.count
+    }
+    /// 초기화 생성 함수 (전체 비트 개수라는 것은 length of bit string 지칭)
+    init(with total: Int) {
+        var newValues = Array<String>()
+        let numberOfValues = NSDecimalNumber(decimal: pow(2, total)).intValue
+        for number in 0...(numberOfValues - 1) {
+            if let binaryValue = Int(String(number, radix: 2)) {
+                newValues.append(String(format: "%05d", binaryValue))
+            }
+        }
+        values = newValues
+    }
+    
+    func find(by bitcount: Int) -> Array<String> {
+        let filteredArray = values.filter { $0.split(separator: "0").joined() == String(repeating: "1", count: bitcount) }
+        return filteredArray
+    }
+}

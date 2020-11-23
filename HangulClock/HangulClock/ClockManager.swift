31825464602f0a253556c4969dc20de97dcdd3db
1song2 <1song2918@gmail.com>
2020-11-24 00:23:28 +0900

    Move codes to ClockManager.swift

--- HangulClock/HangulClock/ClockManager.swift
+++ HangulClock/HangulClock/ClockManager.swift
@@ -7,163 +7,134 @@
 
 import Foundation
 
-struct ClockManager {
-    
-    struct Seconds {
-        let seconds: Int
-        var tensPlace: String {
-            switch seconds / 10 {
-            case 1:
-                return "십"
-            case 2:
-                return "이십"
-            case 3:
-                return "삼십"
-            case 4:
-                return "사십"
-            case 5:
-                return "오십"
-            default:
-                return ""
-            }
-        }
-        var onesPlcae: String {
-            switch seconds % 10 {
-            case 0 where seconds == 0:
-                return "정각"
-            case 1:
-                return "일초"
-            case 2:
-                return "이초"
-            case 3:
-                return "삼초"
-            case 4:
-                return "사초"
-            case 5:
-                return "오초"
-            case 6:
-                return "육초"
-            case 7:
-                return "칠초"
-            case 8:
-                return "팔초"
-            case 9:
-                return "구초"
-            default:
-                return "초"
-            }
+struct Hour {
+    let value: Int
+    var tensPlace: Int? {
+        if value > 21 || (value > 0 && value < 13) {
+            return 1
+        } else {
+            return nil
         }
     }
-    
-    //if hour > 21 {
-    //    //22, 23, 24
-    //    switch hour % 10 {
-    //    case 4:
-    //        hoursLabelArray[0].alpha = 1.0
-    //    case 3:
-    //        hoursLabelArray[1].alpha = 1.0
-    //        hoursLabelArray[2].alpha = 1.0
-    //    case 2:
-    //        hoursLabelArray[1].alpha = 1.0
-    //    default:
-    //        break
-    //    }
-    //} else if hour > 9 && hour < 13 {
-    //    //10, 11, 12
-    //    hoursLabelArray[1].alpha = 1.0
-    //    switch hour % 10 {
-    //    case 0:
-    //        return
-    //    case 1:
-    //        hoursLabelArray[2].alpha = 1.0
-    //    case 2:
-    //        hoursLabelArray[3].alpha = 1.0
-    //    default:
-    //        break
-    //    }
-    //} else {
-    //    //1, 2, 3, 4, 5, 6, 7, 8, 9 //13, 14, 15, 16, 17, 18, 19, 20, 21
-    //    hoursLabelArray[1].alpha = 0.1
-    //    switch (hour - 2) % 10 {
-    //    case 1:
-    //        hoursLabelArray[2].alpha = 1.0
-    //    case 2:
-    //        hoursLabelArray[3].alpha = 1.0
-    //    case 3:
-    //        hoursLabelArray[4].alpha = 1.0
-    //    case 4:
-    //        hoursLabelArray[5].alpha = 1.0
-    //    case 5:
-    //        hoursLabelArray[6].alpha = 1.0
-    //        hoursLabelArray[8].alpha = 1.0
-    //    case 6:
-    //        hoursLabelArray[7].alpha = 1.0
-    //        hoursLabelArray[8].alpha = 1.0
-    //    case 7:
-    //        hoursLabelArray[9].alpha = 1.0
-    //        hoursLabelArray[10].alpha = 1.0
-    //    case 8:
-    //        hoursLabelArray[11].alpha = 1.0
-    //        hoursLabelArray[12].alpha = 1.0
-    //    case 9:
-    //        hoursLabelArray[13].alpha = 1.0
-    //        hoursLabelArray[14].alpha = 1.0
-    //    default:
-    //        break
-    //    }
-    //}
-    
-    //mutating func getSecondsText(_ seconds: Int) {
-    //    if seconds == 0 {
-    //        //secondsLabelArray[0].isHidden = true
-    //        onesPlcae = "정각"
-    //    } else {
-    //        if seconds / 10 >= 1 {
-    //            switch seconds / 10 {
-    //            case 0:
-    //                tensPlace = ""
-    //            case 1:
-    //                tensPlace = "십"
-    //            case 2:
-    //                tensPlace = "이십"
-    //            case 3:
-    //                tensPlace = "삼십"
-    //            case 4:
-    //                tensPlace = "사십"
-    //            case 5:
-    //                tensPlace = "오십"
-    //            default:
-    //                tensPlace = ""
-    //            }
-    //        } else {
-    //            tensPlace = ""
-    //        }
-    //        //secondsLabelArray[0].isHidden = false
-    //        switch seconds % 10 {
-    //        case 0:
-    //            onesPlcae = "초"
-    //        case 1:
-    //            onesPlcae = "일초"
-    //        case 2:
-    //            onesPlcae = "이초"
-    //        case 3:
-    //            onesPlcae = "삼초"
-    //        case 4:
-    //            onesPlcae = "사초"
-    //        case 5:
-    //            onesPlcae = "오초"
-    //        case 6:
-    //            onesPlcae = "육초"
-    //        case 7:
-    //            onesPlcae = "칠초"
-    //        case 8:
-    //            onesPlcae = "팔초"
-    //        case 9:
-    //            onesPlcae = "구초"
-    //        default:
-    //            onesPlcae = ""
-    //        }
-    //    }
-    //
-    //}
+    var onesPlace: (Int, Int?) {
+        //24시는 0시
+        if value == 0 {
+            return (0, nil)
+        }
+        switch value {
+        case 1, 11, 13, 23:
+            return (2, nil)
+        case 2, 12, 14:
+            return (3, nil)
+        case 3, 15:
+            return (4, nil)
+        case 4, 16:
+            return (5, nil)
+        case 5, 17:
+            return (6, 8)
+        case 6, 18:
+            return (7, 8)
+        case 7, 19:
+            return (9, 10)
+        case 8, 20:
+            return (11, 12)
+        case 9, 21:
+            return (13, 14)
+        default:
+            return (0, 0)
+        }
+    }
 }
+
+struct Minutes {
+    let value: Int
+    var tensPlace: (Int, Int?) {
+        if value / 10 >= 1 {
+            switch value / 10 {
+            case 2:
+                return (4, 0)
+            case 3:
+                return (4, 1)
+            case 4:
+                return (4, 2)
+            case 5:
+                return (4, 3)
+            default:
+                return (4, nil)
+            }
+        }
+        return (0, nil)
+    }
+    
+    var onesPlcae: Int {
+        switch value % 10 {
+        case 1:
+            return 5
+        case 2:
+            return 6
+        case 3:
+            return 7
+        case 4:
+            return 8
+        case 5:
+            return 9
+        case 6:
+            return 10
+        case 7:
+            return 11
+        case 8:
+            return 12
+        case 9:
+            return 13
+        default:
+            return 0
+        }
+    }
+}
+
+struct Seconds {
+    let value: Int
+    var tensPlace: String {
+        switch value / 10 {
+        case 1:
+            return "십"
+        case 2:
+            return "이십"
+        case 3:
+            return "삼십"
+        case 4:
+            return "사십"
+        case 5:
+            return "오십"
+        default:
+            return ""
+        }
+    }
+    var onesPlcae: String {
+        switch value % 10 {
+        case 0 where value == 0:
+            return "정각"
+        case 1:
+            return "일초"
+        case 2:
+            return "이초"
+        case 3:
+            return "삼초"
+        case 4:
+            return "사초"
+        case 5:
+            return "오초"
+        case 6:
+            return "육초"
+        case 7:
+            return "칠초"
+        case 8:
+            return "팔초"
+        case 9:
+            return "구초"
+        default:
+            return "초"
+        }
+    }
+}
+
--- HangulClock/HangulClock/ViewController.swift
+++ HangulClock/HangulClock/ViewController.swift
@@ -5,8 +5,6 @@
 //  Created by Song on 2020/11/23.
 //
 
-//TODO: - ClockManager로 모델 분리
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -19,12 +17,10 @@ class ViewController: UIViewController {
     @IBOutlet var secondsLabelArray: [UILabel]!
     
     let calendar = Calendar.current
+    var currentHour: Int { calendar.component(.hour, from: Date()) }
+    var currentMinutes: Int { calendar.component(.minute, from: Date()) }
+    var currentSeconds: Int { calendar.component(.second, from: Date()) }
     var timer = Timer()
-    var hour: Int { calendar.component(.hour, from: Date()) }
-    var minutes: Int { calendar.component(.minute, from: Date()) }
-    var seconds: Int { calendar.component(.second, from: Date()) }
-    
-    //var clockManager = ClockManager(seconds: seconds)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,232 +43,42 @@ class ViewController: UIViewController {
     }
     
     func updateUI(for array: [UILabel]) {
-        (array[0].alpha, array[1].alpha) = (hour > 12) ? (0.1, 1.0) : (1.0, 0.1)
+        (array[0].alpha, array[1].alpha) = (currentHour > 12) ? (0.1, 1.0) : (1.0, 0.1)
     }
     
     func updateUI(for imageView: UIImageView) {
-        let sfSymbol = (hour >= 7 || hour < 19) ? "sun.max.fill" : "moon.fill"
+        let sfSymbol = (currentHour >= 7 && currentHour < 19) ? "sun.max.fill" : "moon.fill"
         imageView.image = UIImage(systemName: sfSymbol)
     }
     
     func updateHourUI() {
         clearUI(array: hoursLabelArray)
-        
-        //let hourDigits = "\(hour)".compactMap { $0.wholeNumberValue }
-        //clearUI(array: hoursLabelArray)
-        //if hour > 21 {
-        //    switch hourDigits[1] {
-        //    case 4:
-        //        hoursLabelArray[0].alpha = 1.0
-        //    case 3:
-        //        hoursLabelArray[1].alpha = 1.0
-        //        hoursLabelArray[2].alpha = 1.0
-        //    case 2:
-        //        hoursLabelArray[1].alpha = 1.0
-        //    default:
-        //        break
-        //    }
-        //} else if hour > 12 {
-        //    hoursLabelArray[1].alpha = 0.1
-        //    switch hourDigits[1] - 2 {
-        //    case 1:
-        //        hoursLabelArray[2].alpha = 1.0
-        //    case 2:
-        //        hoursLabelArray[3].alpha = 1.0
-        //    case 3:
-        //        hoursLabelArray[4].alpha = 1.0
-        //    case 4:
-        //        hoursLabelArray[5].alpha = 1.0
-        //    case 5:
-        //        hoursLabelArray[6].alpha = 1.0
-        //        hoursLabelArray[8].alpha = 1.0
-        //    case 6:
-        //        hoursLabelArray[7].alpha = 1.0
-        //        hoursLabelArray[8].alpha = 1.0
-        //    case 7:
-        //        hoursLabelArray[9].alpha = 1.0
-        //        hoursLabelArray[10].alpha = 1.0
-        //    case 8:
-        //        hoursLabelArray[11].alpha = 1.0
-        //        hoursLabelArray[12].alpha = 1.0
-        //    case 9:
-        //        hoursLabelArray[13].alpha = 1.0
-        //        hoursLabelArray[14].alpha = 1.0
-        //    default:
-        //        break
-        //    }
-        //} else if hour > 9 {
-        //    hoursLabelArray[1].alpha = 1.0
-        //    switch hourDigits[0] {
-        //    case 0:
-        //        return
-        //    case 1:
-        //        hoursLabelArray[2].alpha = 1.0
-        //    case 2:
-        //        hoursLabelArray[3].alpha = 1.0
-        //    default:
-        //        break
-        //    }
-        //} else {
-        //    // hour > 12와 같음 (코드 따로 빼서 처리)
-        //    hoursLabelArray[1].alpha = 0.1
-        //    switch hourDigits[1] - 2 {
-        //    case 1:
-        //        hoursLabelArray[2].alpha = 1.0
-        //    case 2:
-        //        hoursLabelArray[3].alpha = 1.0
-        //    case 3:
-        //        hoursLabelArray[4].alpha = 1.0
-        //    case 4:
-        //        hoursLabelArray[5].alpha = 1.0
-        //    case 5:
-        //        hoursLabelArray[6].alpha = 1.0
-        //        hoursLabelArray[8].alpha = 1.0
-        //    case 6:
-        //        hoursLabelArray[7].alpha = 1.0
-        //        hoursLabelArray[8].alpha = 1.0
-        //    case 7:
-        //        hoursLabelArray[9].alpha = 1.0
-        //        hoursLabelArray[10].alpha = 1.0
-        //    case 8:
-        //        hoursLabelArray[11].alpha = 1.0
-        //        hoursLabelArray[12].alpha = 1.0
-        //    case 9:
-        //        hoursLabelArray[13].alpha = 1.0
-        //        hoursLabelArray[14].alpha = 1.0
-        //    default:
-        //        break
-        //    }
-        //}
+        //let hourManager = Hour(value: currentHour)
+        if let safeTensPlace = Hour(value: currentHour).tensPlace {
+            hoursLabelArray[safeTensPlace].alpha = 1.0
+        }
+        hoursLabelArray[Hour(value: currentHour).onesPlace.0].alpha = 1.0
+        if let safeOnesPlace = Hour(value: currentHour).onesPlace.1 {
+            hoursLabelArray[safeOnesPlace].alpha = 1.0
+        }
     }
     
     func updateMinutesUI() {
-        let minutesDigits = "\(minutes)".compactMap { $0.wholeNumberValue }
         clearUI(array: minutesLabelArray)
-        if minutesDigits.count > 1 {
-            minutesLabelArray[4].alpha = 1.0
-            switch minutesDigits[0] {
-            case 2:
-                minutesLabelArray[0].alpha = 1.0
-            case 3:
-                minutesLabelArray[1].alpha = 1.0
-            case 4:
-                minutesLabelArray[2].alpha = 1.0
-            case 5:
-                minutesLabelArray[3].alpha = 1.0
-            default:
-                break
-            }
-            switch minutesDigits[1] {
-            case 1:
-                minutesLabelArray[5].alpha = 1.0
-            case 2:
-                minutesLabelArray[6].alpha = 1.0
-            case 3:
-                minutesLabelArray[7].alpha = 1.0
-            case 4:
-                minutesLabelArray[8].alpha = 1.0
-            case 5:
-                minutesLabelArray[9].alpha = 1.0
-            case 6:
-                minutesLabelArray[10].alpha = 1.0
-            case 7:
-                minutesLabelArray[11].alpha = 1.0
-            case 8:
-                minutesLabelArray[12].alpha = 1.0
-            case 9:
-                minutesLabelArray[13].alpha = 1.0
-            default:
-                break
-            }
-        } else {
-            switch minutesDigits[0] {
-            case 0:
-                minuteUnitLabel.alpha = 0.1
-            case 1:
-                minutesLabelArray[5].alpha = 1.0
-            case 2:
-                minutesLabelArray[6].alpha = 1.0
-            case 3:
-                minutesLabelArray[7].alpha = 1.0
-            case 4:
-                minutesLabelArray[8].alpha = 1.0
-            case 5:
-                minutesLabelArray[9].alpha = 1.0
-            case 6:
-                minutesLabelArray[10].alpha = 1.0
-            case 7:
-                minutesLabelArray[11].alpha = 1.0
-            case 8:
-                minutesLabelArray[12].alpha = 1.0
-            case 9:
-                minutesLabelArray[13].alpha = 1.0
-            default:
-                break
-            }
+        minuteUnitLabel.alpha = (currentMinutes == 0) ? 0.1 : 1.0
+        //let minutesManager = Minutes(value: currentMinutes)
+        minutesLabelArray[Minutes(value: currentMinutes).tensPlace.0].alpha = 1.0
+        if let safeTensPlace = Minutes(value: currentMinutes).tensPlace.1 {
+            minutesLabelArray[safeTensPlace].alpha = 1.0
         }
+        minutesLabelArray[Minutes(value: currentMinutes).onesPlcae].alpha = 1.0
     }
     
     func updateSecondUI() {
-        let clockManager = ClockManager.Seconds(seconds: seconds)
-        //clockManager.getSecondsText(seconds)
-        if seconds == 0 {
-            secondsLabelArray[0].isHidden = true
-            //secondsLabelArray[1].text = "정각"
-            secondsLabelArray[1].text = clockManager.onesPlcae
-        } else {
-            secondsLabelArray[0].isHidden = false
-            secondsLabelArray[0].text = clockManager.tensPlace
-            secondsLabelArray[1].text = clockManager.onesPlcae
-            //switch seconds / 10 {
-            //case 0:
-            //    secondsLabelArray[0].text = ""
-            //case 1:
-            //    secondsLabelArray[0].text = "십"
-            //case 2:
-            //    secondsLabelArray[0].text = "이십"
-            //case 3:
-            //    secondsLabelArray[0].text = "삼십"
-            //case 4:
-            //    secondsLabelArray[0].text = "사십"
-            //case 5:
-            //    secondsLabelArray[0].text = "오십"
-            //case 6:
-            //    secondsLabelArray[0].text = "육십"
-            //case 7:
-            //    secondsLabelArray[0].text = "칠십"
-            //case 8:
-            //    secondsLabelArray[0].text = "팔십"
-            //case 9:
-            //    secondsLabelArray[0].text = "구십"
-            //default:
-            //    break
-            //}
-            //switch seconds % 10 {
-            //case 0:
-            //    secondsLabelArray[1].text = "초"
-            //case 1:
-            //    secondsLabelArray[1].text = "일초"
-            //case 2:
-            //    secondsLabelArray[1].text = "이초"
-            //case 3:
-            //    secondsLabelArray[1].text = "삼초"
-            //case 4:
-            //    secondsLabelArray[1].text = "사초"
-            //case 5:
-            //    secondsLabelArray[1].text = "오초"
-            //case 6:
-            //    secondsLabelArray[1].text = "육초"
-            //case 7:
-            //    secondsLabelArray[1].text = "칠초"
-            //case 8:
-            //    secondsLabelArray[1].text = "팔초"
-            //case 9:
-            //    secondsLabelArray[1].text = "구초"
-            //default:
-            //    break
-            //}
-        }
+        //let secondManager = Seconds(value: seconds)
+        secondsLabelArray[0].isHidden = currentSeconds == 0
+        secondsLabelArray[0].text = Seconds(value: currentSeconds).tensPlace
+        secondsLabelArray[1].text = Seconds(value: currentSeconds).onesPlcae
     }
     
     func clearUI(array: [UILabel]) {
